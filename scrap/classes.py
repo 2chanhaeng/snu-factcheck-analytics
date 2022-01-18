d08acf461fe72e16c0e9700f3c220ada4625a9e9
chomu37 <2chanhaeng@gmail.com>
2022-01-18 10:04:30 +0900

    Updated classes
    주석 추가

--- scrap/classes.py
+++ scrap/classes.py
@@ -15,7 +15,7 @@ saving_path = "scrap/speakings.yaml"
 class Speaking:
     def __init__(self, num: int, predata: Optional[Dict[Any, Any]] = None):
         self.num = num
-        if predata is None:
+        if predata is None: # id를 이용해 정보를 요청합니다.
             self.responce = requests.get(url % self.num)
             if self.responce.status_code != 200:
                 raise Exception(f"{self.responce.status_code} error at page {self.num}.")
@@ -31,13 +31,14 @@ class Speaking:
             self.cartegories = {li.text for li in self.detail.select(".fcItem_detail_bottom li")} # 카테고리를 추출합니다.
             self.explain = self.soup.select_one(".exp").text.strip() # 설명을 추출합니다.
             self.factchecks = self.get_fc(self.soup)
-        else:
+        else: # 기존 데이터를 로드해서 객체를 다시 생성합니다.
             self.speacker = predata['speacker']
             self.title = predata['title']
             self.source = predata['source']
             self.cartegories = predata['cartegories']
             self.explain = predata['explain']
             self.factchecks = predata['factchecks']
+    
     
     def as_dict(self): # 데이터를 딕셔너리 형태로 반환합니다.
         return {
@@ -49,10 +50,12 @@ class Speaking:
             'factchecks': self.factchecks
         }
     
-    def as_yaml(self): # 데이터를 yaml 형태로 반환합니다.
+
+    def as_yaml(self): # 데이터를 yaml 형태로 변환합니다.
         return yaml.dump(self.as_dict(), allow_unicode=True)
 
-    def save_as_yaml(self, path:str):
+
+    def save_as_yaml(self, path:str): # 데이터를 yaml 형태로 저장합니다.
         with open(path, 'w') as f:
             f.write(self.as_yaml())
 
@@ -61,23 +64,19 @@ class Speaking:
     # <script charset="utf-8" type="text/javascript">
     # $(function () { showScore(아이디, 점수)}); </script> ... </ul>
     # 따라서 아이디와 점수를 추출하는 정규표현식을 사용합니다.
+    id_score_re = re.compile(r'(?<=showScore\()\d+, \d+')
 
-    
     # 아이디와 점수를 추출하는 함수를 정의합니다.
-
     def get_fc_id_score(self, soup:bs) -> List[Tuple[int, int]]:
         id_score: List[Tuple[int,int]] = [] # 아이디와 점수를 저장할 리스트
-        id_score_re = re.compile(r'(?<=showScore\()\d+, \d+')
         for fc_item_script in soup.select(".fcItem_vf > ul > script"): # 팩트 체크 아이템의 스크립트를 찾습니다.
-            id_score_str: str = id_score_re.search(fc_item_script.text).group() # 정규 표현식으로 아이디와 점수값을 담은 문자열을 추출합니다.
+            id_score_str: str = Speaking.id_score_re.search(fc_item_script.text).group() # 정규 표현식으로 아이디와 점수값을 담은 문자열을 추출합니다.
             id_score_int: Tuple = tuple(map(int, id_score_str.split(', '))) # 문자열을 정수로 변환합니다.
             id_score.append(id_score_int)
         return id_score
 
 
-
     # 작성 시간과 내용을 추출하는 함수를 정의합니다.
-
     def get_fc_time_contents(self, soup:bs) -> List[Tuple[str, str, str]]:
         time_contents: List[str] = [] # 시간과 내용을 저장할 리스트
         for fc_item in soup.select(".fcItem_vf > ul > li"): # 팩트 체크 아이템을 찾습니다.
@@ -88,8 +87,8 @@ class Speaking:
             time_contents.append((date, time, content))
         return time_contents
 
-    # 위의 함수들의 반환을 합쳐 최종적으로 아이디를 키로 갖고 나머지 내용을 값으로 갖는 딕셔너리를 반환하는 함수를 정의합니다.
 
+    # 위의 함수들의 반환을 합쳐 최종적으로 아이디를 키로 갖고 나머지 내용을 값으로 갖는 딕셔너리를 반환하는 함수를 정의합니다.
     def get_fc(self, soup:bs) -> Dict[int, Dict[Any, Any]]:
         id_score = self.get_fc_id_score(soup) # 아이디와 점수를 추출합니다.
         time_contents = self.get_fc_time_contents(soup) # 작성 시간과 내용을 추출합니다.
@@ -102,20 +101,22 @@ class Speaking:
     def scrap_speaking(how_many:int = 4000, stop_when_errors_continued: int = 20) -> List['Speaking']:
         speakings: List[Speaking] = []
         errors: Set[int] = set()
-        continued_errors: int = 0
+        continued_errors: int = 0 # 연속된 에러가 발생한 횟수를 저장합니다.
         
         for i in tqdm(range(1,how_many + 1)):
             try:
                 speakings.append(Speaking(i))
-                continued_errors = 0
+                continued_errors = 0 # 에러가 없으면 에러 횟수를 0으로 초기화합니다.
             except:
+
+                # 에러가 발생하면 에러를 저장하고 연속된 에러 횟수를 증가시킵니다. 
                 errors.add(i)
                 continued_errors += 1
-                if continued_errors > stop_when_errors_continued:
+
+                # 연속된 에러가 정해진 횟수를 넘어가면 종료합니다.
+                if continued_errors > stop_when_errors_continued: 
                     print(f"{i}번째 페이지까지 스크래핑하는데 에러가 연속으로 {continued_errors}번 발생했습니다. 스크래핑을 중단합니다.")
                     break
-            if i % 10 == 0:
-                sleep(1)
         print(f"업데이트된 데이터의 수는 {len(speakings)}/{how_many}개 입니다.")
         return speakings
 
@@ -123,9 +124,9 @@ class Speaking:
     # 스크래핑한 Speaking 객체들의 데이터를 저장하는 함수를 정의합니다.
     @staticmethod
     def save_speaking(data: Any, file_name: str = saving_path):
-        if type(data) is not str:
+        if type(data) is not str: # 데이터가 문자열이 아니면 yaml 형식으로 변환합니다.
             yaml.dump(data, open(file_name, 'w', encoding="utf-8"), default_flow_style=False, allow_unicode=True)
-        else:
+        else: # 데이터가 문자열이면 그대로 저장합니다.
             with open(file_name, 'w', encoding="utf-8") as f:
                 f.write(data)
 
@@ -134,9 +135,9 @@ class Speaking:
     @staticmethod
     def load_speakings_as_dict(file_name: str = saving_path) -> Dict[Any, Any]:
         speaks_dict = yaml.load(open(file_name, 'r', encoding="utf-8"), Loader=yaml.FullLoader)
-        if type(speaks_dict) is dict:
+        if type(speaks_dict) is dict: # 불러온 데이터가 딕셔너리면 그대로 반환합니다.
             return speaks_dict
-        else:
+        else: # 불러온 데이터가 딕셔너리가 아니면 에러를 발생시킵니다.
             raise TypeError(f"{file_name} 이 딕셔너리로 저장되지 않았습니다. 현재 저장된 타입은 {type(speaks_dict)} 입니다.")
 
 
@@ -151,8 +152,8 @@ class Speaking:
     # 저장되어 있는 Speaking 데이터 베이스를 업데이트하는 함수를 정의합니다.
     @staticmethod
     def update_speakings(file_name: str = saving_path, how_many: int = 100, stop_when_errors_continued: int = 10):
-        from os.path import exists
         # file_name 파일이 존재하면 파일을 로드하여 가장 큰 key 부터, 없으면 0부터 시작합니다.
+        from os.path import exists
         if exists(file_name):
             speaks_dict = Speaking.load_speakings_as_dict(file_name)
             last_id =  max(speaks_dict.keys())
